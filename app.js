1610e5e49e6ee72f0bb50bc143b7154f4043378b
sanikapol <50167925+sanikapol@users.noreply.github.com>
2020-09-10 19:17:59 -0400

    Created simple NodeJs server

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+const http = require('http')
+const port = 5000
+
+const server = http.createServer(function(request,response){
+    response.write('Hello World!!')
+    response.end()
+})
+//console.log("Hello World");
+
+server.listen(port, function(error){
+    if(error){
+        console.log('Something went wrong', error)
+    }
+    else{
+        console.log('Server is listening on port ' + port)
+    }
+})
